3e2e7a8933f03bd75d4fd57e14a5976a4cd020ee
juan <em.rodas21@ciaf.edu.co>
2024-10-05 12:00:56 -0500

    Termino la clase

---
+++ IntroduccionJS/app.js
@@ -0,0 +1,16 @@
+console.log("hello world")
+let person = {
+    height: 175,
+    weight: 70,
+
+}
+console.table(person);
+console.log('integer $(10');
+console.log("string");
+console.log('objetr %(person)');
+console.log('bolean $(True)');
+
+const fruits = ["apple", "banana", "cherry"];
+
+// metodos asocidos a los strings
+const phrase = "Hola como etas"
