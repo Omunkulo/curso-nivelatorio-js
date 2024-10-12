c4b610b0adc8bca55585c0338a69f7dd9384ae16
juan <em.rodas21@ciaf.edu.co>
2024-10-12 11:53:15 -0500

    acabo segunda clase pe

--- Introduccion/app.js
+++ Introduccion/app.js
@@ -2,7 +2,17 @@ let btn = document.getElementById("main_button");
 let title = document.getElementById("main_titulo");
 btn.addEventListener('click', function(){
     currentTitle = title.textContent;
-    title.textContent = currentTitle.toUpperCase();
+
+    if (currentTitle === currentTitle.toUpperCase()){
+        main_button.textContent = currentTitle.toLowerCase();
+    } else {
+        main_button.textContent = currentTitle.toUpperCase();
+    }
 });
 
+let sum =0;
+for (let 1 = 0; i<10; i++){
+    sum+=i;
+}
+console.log(sum);
 //let main:div = document.querySelector('main_div')
